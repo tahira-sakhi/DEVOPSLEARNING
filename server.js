322180b6fdd255194d8c00590dce005c40f56501
tahira-sakhi <tahi.parveen93@gmail.com>
2024-04-26 17:19:00 +0500

    updated

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
